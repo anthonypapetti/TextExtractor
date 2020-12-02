c5d4cfbe3653e258ffdca470d490b6dc5511c104
anthonypapetti <apapetti@outlook.com>
2020-12-02 17:15:56 +0000

    Created "skeleton" of web app

--- run.py
+++ run.py
@@ -1,37 +1,9 @@
-import sys
-import os
-from counts import charcount, linecount, specialcount, wordcount
-from sorts import commonsort, alphasort
+from flask import Flask, render_template
+app = Flask(__name__)
 
-if len(sys.argv) != 2:
-    raise Exception("Usage: python run.py filename.txt")
+@app.route('/')
+def index():
+    return render_template("index.html")
 
-file_name, file_extension = os.path.splitext(sys.argv[1])
-
-if file_extension != ".txt":
-    raise Exception("Please use only plaintext files")
-
-infile = open(sys.argv[1], "r")
-
-#create return files
-metadata_outfile = open("metadata.txt", "w")
-commonsort_outfile = open("commonsort.txt", "w")
-alphasort_outfile = open("alphasort.txt", "w")
-
-#write to metadata file
-metadata_outfile.write(f"Characters- {charcount(infile)}\n")
-metadata_outfile.write(f"Characters (without spaces)- {charcount(infile, False)}\n")
-metadata_outfile.write(f"Lines- {linecount(infile)}\n")
-metadata_outfile.write(f"Special Punctuation- {specialcount(infile)}\n")
-metadata_outfile.write(f"Words- {wordcount(infile)}\n")
-
-#write to commonsort file
-common_words = commonsort(infile)
-for word in common_words:
-    values = list(word.values())
-    commonsort_outfile.write(f"{values[0]}- {values[1]}\n")
-
-#TODO: Write to alphabeticalsort outfile
-alphawords = alphasort(infile)
-for word in alphawords:
-    alphasort_outfile.write(f"{word}\n")
+if __name__ == "__main__":
+  app.run(host='0.0.0.0')
