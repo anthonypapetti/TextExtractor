5ef3994f703855d2f3b2ad1c345191b1e92b80b8
anthonypapetti <apapetti@outlook.com>
2020-12-05 10:53:18 -0500

    Created basic layout

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request
 from counts import charcount, specialcount, wordcount
+import datetime
 app = Flask(__name__)
 app.config["DEBUG"] = True
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -9,7 +11,8 @@ def index():
 		text = request.form["text"]
 		return render_template("index.html")
 	if request.method == "GET":
-		return render_template("index.html")
+		data = 1
+		return render_template("index.html", data=data)
 
 if __name__ == "__main__":
 	app.run()
